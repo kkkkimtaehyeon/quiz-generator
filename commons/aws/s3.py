8d261b4021ef45903c35068720512d465b826ce5
kkkkimtaehyeon <kth1927@naver.com>
2024-06-09 22:22:56 +0900

    pdf 읽는 함수 추가

--- commons/aws/s3.py
+++ commons/aws/s3.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+import io
 import uuid
 from dotenv import load_dotenv, find_dotenv
 
@@ -18,6 +19,10 @@ def upload_pdf(file):
     key = generate_objectkey(file)
     s3.put_object(Body=file.file, Bucket=S3_BUCKET, Key=key, ContentType='application/pdf')
     return f'https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/{key}'
+
+def get_pdf_file_stream(key):
+    obj = s3.get_object(Bucket=S3_BUCKET, Key=key)
+    return io.BytesIO(obj['Body'].read())
 
 def generate_objectkey(file):
     filename = file.filename
